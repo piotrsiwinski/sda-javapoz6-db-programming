dc103559335dd3841c819ece10587a3797e312a2
Piotr Siwinski <piotr.siwinski@f-secure.com>
2018-04-28 13:14:22 +0200

    Added test query

--- jdbc-app/src/main/java/pl/sda/poznan/Program.java
+++ jdbc-app/src/main/java/pl/sda/poznan/Program.java
@@ -1,7 +1,18 @@
 package pl.sda.poznan;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import pl.sda.poznan.util.DbUtils;
+
 public class Program {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws SQLException {
+    ResultSet rs = DbUtils.getConnection().createStatement()
+        .executeQuery("SELECT COUNT(ID) FROM users");
+
+    rs.next();
+    int count = rs.getInt(1);
+    System.out.println(count);
   }
 }
