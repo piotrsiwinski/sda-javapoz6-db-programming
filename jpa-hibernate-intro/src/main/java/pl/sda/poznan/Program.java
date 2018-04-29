f294af39410cd3cacbf31bcb9555cfec846e0150
Piotr Siwinski <piotr.siwinski@f-secure.com>
2018-04-29 14:49:19 +0200

    Add sample queries

--- jpa-hibernate-intro/src/main/java/pl/sda/poznan/Program.java
+++ jpa-hibernate-intro/src/main/java/pl/sda/poznan/Program.java
@@ -1,17 +1,34 @@
 package pl.sda.poznan;
 
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 import pl.sda.poznan.model.Category;
 import pl.sda.poznan.model.Product;
 
 public class Program {
 
+
   public static void main(String[] args) {
     EntityManagerFactory factory = Persistence.createEntityManagerFactory("UsersDB");
     EntityManager entityManager = factory.createEntityManager();
+    seedData(entityManager);
+    // get all products
 
+    Query query = entityManager.createQuery("from Product");
+    List resultList = query.getResultList();
+
+    // Find by id
+    Query getByIdQuery = entityManager.createQuery("select p from Product p where p.id = :productId");
+    getByIdQuery.setParameter("productId", 2L);
+    Product singleResult = (Product) getByIdQuery.getSingleResult();
+
+
+  }
+
+  private static void seedData(EntityManager entityManager) {
     // todo 1 - kategoria
     Category category = new Category();
     category.setName("Laptops");
